5b9f5ce437f878b8b951ddbd156c1a1dccbefecc
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-04-02 18:42:23 +0900

    update `bake_test.py`

--- tests/bake_test.py
+++ tests/bake_test.py
@@ -1,26 +1,10 @@
+import pathlib
 import sys
+from typing import Dict
 
 import pytest
 import yaml
 from pytest_cookies.plugin import Cookies
-
-
-@pytest.fixture
-def dataset_name() -> str:
-    return "TestHfDataset"
-
-
-@pytest.fixture
-def citation() -> str:
-    return """\
-@misc{author_year,
-  title={TestDataset},
-  author={Author Name},
-  year={Year},
-  howpublished={Publisher},
-  note={URL or other relevant information}
-}
-"""
 
 
 @pytest.fixture
@@ -29,33 +13,25 @@ def python_version() -> str:
 
 
 @pytest.fixture
-def description() -> str:
-    return "TestHfDataset is a sample dataset for artificial intelligence experiments and evaluations, containing labeled images for image recognition tasks."
+def project_root_dir() -> pathlib.Path:
+    return pathlib.Path(__file__).parents[1]
 
 
 @pytest.fixture
-def homepage() -> str:
-    return "https://www.testdataset.com"
+def test_fixtures_dir(project_root_dir: pathlib.Path) -> pathlib.Path:
+    return project_root_dir / "test_fixtures"
 
 
 @pytest.fixture
-def dataset_license() -> str:
-    return "This dataset is released under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (CC BY-NC-SA 4.0)."
+def config_path(test_fixtures_dir: pathlib.Path) -> pathlib.Path:
+    return test_fixtures_dir / "config.yaml"
 
 
 @pytest.fixture
-def arxiv_url() -> str:
-    return "https://arxiv.org/abs/XXXX.XXXXX"
-
-
-@pytest.fixture
-def publication_url() -> str:
-    return "https://www.journalwebsite.com/paper-title"
-
-
-@pytest.fixture
-def publication_venue() -> str:
-    return "Journal of Cookiecutter Test"
+def test_config(config_path: pathlib.Path) -> Dict[str, str]:
+    with config_path.open("r") as rf:
+        config = yaml.safe_load(rf)
+    return config
 
 
 @pytest.mark.parametrize(
@@ -66,17 +42,15 @@ def test_bake_project(
     cookies: Cookies,
     dataset_name: str,
     python_version: str,
-    citation: str,
-    description: str,
-    homepage: str,
-    dataset_license: str,
-    datasets_type: str,
-    arxiv_url: str,
-    publication_url: str,
-    publication_venue: str,
+    test_config: Dict[str, str],
 ) -> None:
-    result = cookies.bake(extra_context={"python_version": python_version})
-    breakpoint()
+
+    result = cookies.bake(
+        extra_context={
+            **test_config["default_context"],  # type: ignore
+            "python_version": python_version,
+        },
+    )
 
     assert result.exit_code == 0
     assert result.exception is None
@@ -84,5 +58,3 @@ def test_bake_project(
     assert result.project_path is not None
     assert result.project_path.name == f"huggingface-datasets_{dataset_name}"
     assert result.project_path.is_dir()
-
-    breakpoint()
